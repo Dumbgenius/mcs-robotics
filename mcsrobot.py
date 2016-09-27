f5cd691bf48d6eae798546ea7a49d54af2dc206f
Jacob <handsjacob@gmail.com>
2016-09-27 21:26:37 +0100

    Other minor fixes, like changing instances of "location" to "position"
    Disclaimer: I can't actually run this code as I don't have a robot to run it on, and I can't use the IDE, so it's probably all broken.

--- mcsrobot.py
+++ mcsrobot.py
@@ -59,14 +59,14 @@ def get_average_angle(l):
 	totalx = 0
 	totaly = 0
 	for i in l:
-		totalx += cos(radians(i))
-		totaly += sin(radians(i))
-	return degrees(atan2(totaly/totalx))
-	#this method is one i got from stackoverflow:
+		totalx += math.cos(radians(i))
+		totaly += math.sin(radians(i))
+	return math.degrees(math.atan2(totaly/totalx))
+	# this method is one I got from stackoverflow:
 	#	-turn each angle into a unit vector
 	#	-find the direction of their sum
-	#you can't just add them as angles are cyclic and so "average" isn't
-	#even really a meaningful concept for them.
+	# it's like this as you can't just add them since angles are cyclic and so 
+	# "average" isn't even really a meaningful concept for them.
 
 class NotSureWhatHappenedError(Exception):
 	"""The standard error for the MCS Robot."""
@@ -100,20 +100,20 @@ class MCSRobot():
 
 	def get_position(self):
 		markers = self.R.see(res=(1280,720)) #biggest res that works on both cameras
-		locations_x = []
-		locations_y = []
+		positions_x = []
+		positions_y = []
 		rotations = []
 		#first, for each marker, work out my rotation from its (known) rotation
 		#and from how it appears to be rotated relative to me.
 		for m in markers:
 			if m.marker_type == MARKER_ARENA:
-				if MARKER_DIRECTIONS(m.offset) == SOUTH: rot = m.rot_y
-				elif MARKER_DIRECTIONS(m.offset) == WEST: rot = 270+m.rot_y
-				elif MARKER_DIRECTIONS(m.offset) == NORTH: rot = 180+m.rot_y
-				elif MARKER_DIRECTIONS(m.offset) == EAST: rot = 90+m.rot_y
+				if MARKER_DIRECTIONS[m.offset] == SOUTH: rot = m.rot_y
+				elif MARKER_DIRECTIONS[m.offset] == WEST: rot = 270+m.rot_y
+				elif MARKER_DIRECTIONS[m.offset] == NORTH: rot = 180+m.rot_y
+				elif MARKER_DIRECTIONS[m.offset] == EAST: rot = 90+m.rot_y
 				# i *think* the above code works
 				# if it does, remove it and uncomment the next line
-				#rot = m.rot_y + MARKER_DIRECTIONS(m.offset)
+				#rot = m.rot_y + MARKER_DIRECTIONS[m.offset]
 				rotations.append(rot)
 		#then take my rotation as the average
 		self.rotation = get_average_angle(rotations)
@@ -122,12 +122,12 @@ class MCSRobot():
 		for m in markers:
 			if m.marker_type == MARKER_ARENA:
 				my_rotation_relative_to_it = 0-m.rot_y
-				posx = MARKER_LOCATIONS(marker.offset)[0] + m.dist*cos(radians(my_rotation_relative_to_it))
-				posy = MARKER_LOCATIONS(marker.offset)[1] + m.dist*cos(radians(my_rotation_relative_to_it))
-				locations_x.append(posx)
-				locations_y.append(posy)
+				posx = MARKER_LOCATIONS[marker.offset][0] + m.dist*math.cos(radians(my_rotation_relative_to_it))
+				posy = MARKER_LOCATIONS[marker.offset][1] + m.dist*math.cos(radians(my_rotation_relative_to_it))
+				positions_x.append(posx)
+				positions_y.append(posy)
 		#and take my position as the average
-		self.location = (get_average(locations_x), get_average(locations_y))
+		self.position = (get_average(positions_x), get_average(positions_y))
 		#and hey, i'm not sure if this code even works! but hopefully it does.
 
 	def test_speed_bursts(tests=10, seconds_per_test=1, test_speed=100):
@@ -139,7 +139,7 @@ class MCSRobot():
 			pos = self.position #get initial position
 			move_linear(seconds_per_test, test_speed) #move forward
 			get_position() #get new position
-			forward_distances.append(sqrt((self.position[0]-pos[0])**2 + (self.position[1]-pos[1])**2)) #and get the distance
+			forward_distances.append(math.sqrt((self.position[0]-pos[0])**2 + (self.position[1]-pos[1])**2)) #and get the distance
 
 			#...and then do the same, but drive backwards
 			get_position()
@@ -167,7 +167,7 @@ class MCSRobot():
 			pos = self.position #save position
 			time.sleep(delay) #wait (while moving forward)
 			get_position() #get new position
-			speeds.append(sqrt((self.position[0]-pos[0])**2 + (self.position[1]-pos[1])**2) / time.time()-t) #speed = displacement / time
+			speeds.append(math.sqrt((self.position[0]-pos[0])**2 + (self.position[1]-pos[1])**2) / time.time()-t) #speed = displacement / time
 			t = time.time() #set time again
 
 		self.R.motors[0].m0.power = 0
